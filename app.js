2f04de12f672f8328aba3e809874c0fa8e09be88
lhafkamp <Luuk@Luuks-MacBook-Pro.local>
2017-04-22 00:07:23 +0200

    getting data back from the oauth now

--- app.js
+++ app.js
@@ -15,6 +15,40 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// set up all variables needed for oauth
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI;
+const response_type = 'code';
+const scope = 'basic';
+
+// url with all the needed variables
+const auth_url = `https://api.instagram.com/oauth/authorize/?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
+
+// render the index page
+app.get('/', (req, res) => {
+	res.render('index', {
+		auth_url: auth_url
+	});
+});
+
+// get the token once the auth is complete and render succes
+app.get('/succes', (req, res) => {
+	request.post({
+		uri: 'https://api.instagram.com/oauth/access_token',
+		form: {
+			client_id: client_id,
+			client_secret: client_secret,
+			grant_type: 'authorization_code',
+			redirect_uri: redirect_uri,
+			code: req.query.code
+		}
+	}, (error, response, body) => {
+		console.log(body);
+	});
+	res.render('succes');
+});
+
 // 404
 app.get('*', (req, res) => {
 	res.render('error');
