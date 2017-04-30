925a686956789da8c0571da07c7294bcf7a88b49
lhafkamp <luukhafkamp@gmail.com>
2017-04-30 20:15:48 +0200

    its not pretty but the counting works

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13,31 +13,50 @@ socket.on('newPic', (data) => {
 });
 },{}],3:[function(require,module,exports){
 const photo = document.querySelectorAll('.pic img');
+const counter = document.querySelectorAll('.pic p');
 const bad = document.querySelectorAll('button:first-of-type');
 const good = document.querySelectorAll('button:last-of-type');
 
-let score = 100;
-
 function decreaseScore() {
+	const thisParent = this.parentNode.parentNode
+	let score = Number(thisParent.querySelector('p').textContent);
+
 	if (score < 25) {
-		return
+		thisParent.style.display = 'none';
+	} else if (score < 50) {
+		score -= 25;
 	} else {
 		score -= 25;
 	}
+
+
+
+
+
 	this.parentNode.parentNode.querySelector('img').style.filter = `brightness(${score}%)`;
-	console.log('the current score is', score);
+
+
+
+
+	thisParent.querySelector('img').style.filter = `brightness(${score}%)`;
+	thisParent.querySelector('p').innerHTML = `${score}`;
 };
 
 function increaseScore() {
+	const thisParent = this.parentNode.parentNode
+	let score = Number(thisParent.querySelector('p').textContent);
+
 	if (score > 75) {
 		return
 	} else {
 		score += 25;
 	}
-	this.parentNode.parentNode.querySelector('img').style.filter = `brightness(${score}%)`;
-	console.log('the current score is', score);
+
+	thisParent.querySelector('img').style.filter = `brightness(${score}%)`;
+	thisParent.querySelector('p').innerHTML = `${score}`;
 };
 
 bad.forEach(btn => btn.addEventListener('click', decreaseScore));
 good.forEach(btn => btn.addEventListener('click', increaseScore));
+
 },{}]},{},[1]);
